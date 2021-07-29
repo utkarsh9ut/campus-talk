081cf47625b857b79489d8364898b202fcd8a103
utkarsh9ut <utkarshthatai9@gmail.com>
2021-07-29 22:43:56 +0530

    setting commit

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 const express = require('express');
-// const env = require('./config/environment');
-// const logger = require('morgan');
-
-
 const cookieParser = require('cookie-parser');
 const app = express();
-// require('./config/view-helpers')(app);
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
@@ -21,23 +16,14 @@ const sassMiddleware = require('node-sass-middleware');
 const flash = require('connect-flash');
 const customMware = require('./config/middleware');
 
-// setup the chat server to be used with socket.io
-// const chatServer = require('http').Server(app);
-// const chatSockets = require('./config/chat_sockets').chatSockets(chatServer);
-// chatServer.listen(5000);
-console.log('chat server is listening on port 5000');
-const path = require('path');
 
-// if (env.name == 'development'){
-//     app.use(sassMiddleware({
-//         src: path.join(__dirname, env.asset_path, 'scss'),
-//         dest: path.join(__dirname, env.asset_path, 'css'),
-//         debug: true,
-//         outputStyle: 'extended',
-//         prefix: '/css'
-//     }));
-// }
-
+app.use(sassMiddleware({
+    src: './assets/scss',
+    dest: './assets/css',
+    debug: true,
+    outputStyle: 'extended',
+    prefix: '/css'
+}));
 app.use(express.urlencoded());
 
 app.use(cookieParser());
@@ -45,8 +31,6 @@ app.use(cookieParser());
 app.use(express.static('./assets'));
 // make the uploads path available to the browser
 app.use('/uploads', express.static(__dirname + '/uploads'));
-
-// app.use(logger(env.morgan.mode, env.morgan.options));
 
 app.use(expressLayouts);
 // extract style and scripts from sub pages into the layout
@@ -64,7 +48,7 @@ app.set('views', './views');
 app.use(session({
     name: 'codeial',
     // TODO change the secret before deployment in production mode
-    secret: "blahsomething",
+    secret: 'blahsomething',
     saveUninitialized: false,
     resave: false,
     cookie: {
